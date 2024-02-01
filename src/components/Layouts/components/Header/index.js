cb8ef0233a219c7c8c24d780b8cf112be23eb581
CungSao <2022bevy@gmail.com>
2024-02-01 07:34:42 +0700

    UI Header 4

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -26,6 +26,21 @@ const MENU_ITEMS = [
   {
     icon: <FontAwesomeIcon icon={faEarthAsia} />,
     title: 'English',
+    children: {
+      title: 'Language',
+      data: [
+        {
+          type: 'language',
+          code: 'en',
+          title: 'English',
+        },
+        {
+          type: 'language',
+          code: 'vi',
+          title: 'Tiếng Việt',
+        },
+      ],
+    },
   },
   {
     icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -46,6 +61,16 @@ function Header() {
       setSearchResult([]);
     }, 0);
   }, []);
+
+  // Handle logic
+  const handleMenuChange = (menuItem) => {
+    switch (menuItem.type) {
+      case 'language':
+        // Handle change language
+        break;
+      default:
+    }
+  };
 
   return (
     <header className={cx('wrapper')}>
@@ -79,7 +104,7 @@ function Header() {
           {/* <Button primary href='https://fullstack.edu.vn/learning-paths' target="_blank" onClick={() => alert('Log in!')}> */}
           <Button text> Upload </Button>
           <Button primary>Log in</Button>
-          <Menu items={MENU_ITEMS}>
+          <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
             <button className={cx('more-btn')}>
               <FontAwesomeIcon icon={faEllipsisV} />
             </button>
